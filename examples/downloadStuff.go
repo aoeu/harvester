0a1654c64bba33ea6ac6b44734e61100a7255589
Travis <tmcdemus@gmail.com>
2014-10-28 21:35:56 -0400

    Fun bug fixed.

--- examples/downloadStuff.go
+++ examples/downloadStuff.go
@@ -10,10 +10,7 @@ func main() {
 	results := make(chan []byte)
 	errors := make(chan error)
 	url := "http://www.google.com"
-	// This won't return since we didn't start it with the go keyword.
-	// In other words, the unbuffered channel will not be written to
-	// if nothing is yet reading from the unbuffered channel.
-	harvester.Download(url, results, errors)
+	go harvester.Download(url, results, errors)
 	select {
 	case err := <-errors:
 		log.Fatal(err)
