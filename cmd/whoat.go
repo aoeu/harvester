fdfa05fa38aab3e87961b0d1cce4a1f843ce1666
Travis <travis@mcdem.us>
2015-03-18 14:07:17 -0400

    Add downloading of repository contributors

--- cmd/whoat.go
+++ cmd/whoat.go
@@ -1,52 +1,72 @@
-package main 
+package main
 
 import (
-	"fmt"
-	"flag"
-	"log"
-	"net/url"
-	"net/http"
 	"encoding/json"
-	"io/ioutil"
+	"flag"
+	"fmt"
+	"harvester"
+	"log"
 )
 
 type Repos []Repo
 
 type Repo struct {
-	Id int
-	Name string
-	Full_name string
+	Id               int
+	Name             string
+	Full_name        string // TODO(aoeu): Use annotations and idiomatic names
 	Contributors_url string
 }
 
+type Contributors []Contributor
+
+type Contributor struct {
+	Id                int
+	Login             string
+	Url               string
+	Avatar_url        string
+	Organizations_url string
+	Repos_url         string
+	Contributions     int
+}
+
+type R struct {
+	Repo
+	Contributors
+}
 
 var orgName string
 
-func main() { 
+func main() {
 	flag.StringVar(&orgName, "organization name", "hackerschool", "A github organization name.")
 	flag.Parse()
 
-	s := fmt.Sprintf("https://api.github.com/orgs/%s/repos", orgName)
-	reposURL, err := url.ParseRequestURI(s)
+	reposURI := fmt.Sprintf("https://api.github.com/orgs/%s/repos", orgName)
+	body, err := harvester.Download(reposURI)
 	if err != nil {
 		log.Fatal(err)
 	}
-	
-	resp, err := http.Get(reposURL.String())
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer resp.Body.Close()
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		log.Fatal(err)
-	}
-	
+
 	var repos Repos
 	err = json.Unmarshal(body, &repos)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	fmt.Printf("%+v\n", repos)
+	repoBundles := make([]R, len(repos))
+	for i, repo := range repos {
+		repoBundles[i].Repo = repo
+
+		body, err := harvester.Download(repo.Contributors_url)
+		if err != nil {
+			log.Fatal(err)
+		}
+		var c Contributors
+		err = json.Unmarshal(body, &c)
+		if err != nil {
+			log.Fatal(err)
+		}
+		repoBundles[i].Contributors = c
+	}
+
+	fmt.Printf("%+v\n", repoBundles)
 }
